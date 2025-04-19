e3f7854c940c84a7a0702b3403a3564a06c29272
Maksimod <maksumonka@gmail.com>
2025-04-19 19:47:19 +0300

    remove_lis

--- restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Удаляем файл блокировки если существует
-rm -f bot.lock
-
-# Перезапускаем бота
-pm2 restart google_meet_bot
--- run_bot.sh
+++
@@ -1,5 +0,0 @@
-
-pm2 start --name "google_meet_bot" --interpreter=/home/user/test_google_meet/myenv/bin/python bot.py
-
-# Сохраняем конфигурацию PM2
-pm2 save 
